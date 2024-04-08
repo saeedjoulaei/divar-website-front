f9cd7107758b005a90e9c40f8591f38ee1f3abcb
saeed joulaei <saeedjoulaei86@gmail.com>
2024-04-08 19:35:05 +0330

    layout added to project

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Router from "./router/Router";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import LayOut from "./layouts/LayOut";
 
 import defaultOptions from "./configs/reactQueryConfigs";
 
@@ -11,7 +12,9 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
-        <Router />
+        <LayOut>
+          <Router />
+        </LayOut>
       </BrowserRouter>
       <ReactQueryDevtools />
     </QueryClientProvider>
