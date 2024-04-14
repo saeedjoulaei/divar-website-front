669a4f4ac55482766c06d7205b8fd57ea2b490fb
saeed joulaei <saeedjoulaei86@gmail.com>
2024-04-14 15:10:41 +0330

    styles for categorylist added

--- src/components/templates/CategoryList.jsx
+++ src/components/templates/CategoryList.jsx
@@ -2,11 +2,13 @@ import { useQuery } from "@tanstack/react-query";
 import { getCategory } from "../../services/admin";
 import Loader from "../modules/Loader";
 
+import styles from "./CategoryList.module.css";
+
 function CategoryList() {
   const { data, isLoading } = useQuery(["get-categories"], getCategory);
   console.log({ data, isLoading });
   return (
-    <div>
+    <div className={styles.list}>
       {isLoading ? (
         <Loader />
       ) : (
